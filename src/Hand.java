04396c6f0bf8ccadc9762c44b169918aae3b0595
Francis Bolduc <f.bolduc@outlook.com>
2022-08-05 17:27:09 -0400

    hiss

--- src/Hand.java
+++ src/Hand.java
@@ -9,15 +9,15 @@ public class Hand {
         this.user = user;
         hand = new Card[2];
     }
-    
-	//retroune le tableau hand
+
+    //retroune le tableau hand
     public Card[] getCards() {
         return this.hand;
     }
-    
-	// retourne la somme des cartes dans le tableau hand. Si la somme est >21, il faut recompter les cartes pour verifier si il y a un ACE. Si oui on le considere comme 1, sinon on ajoute la somme des points
 
-//    TODO Pourrais regarder d'enlever une loop peut-etre
+    // retourne la somme des cartes dans le tableau hand. Si la somme est >21, il faut recompter les cartes pour verifier si il y a un ACE. Si oui on le considere comme 1, sinon on ajoute la somme des points
+
+    //    TODO Pourrais regarder d'enlever une loop peut-etre
     public int getPoints() {
         int sum = 0;
         for (Card item : hand) {
@@ -34,14 +34,14 @@ public class Hand {
     }
 
 
-    
-	// ajouter une carte au tableau
+
+    // ajouter une carte au tableau
     public void addCard(Card card) {
 //      hand[i++] = new Card(card)
         hand[i++] = card;
     }
-    
-	//retourne true si la somme de deux cartes est égale à 21. False sinon
+
+    //retourne true si la somme de deux cartes est égale à 21. False sinon
     public boolean isBlackjack() {
         int sum = 0;
         for (Card card: hand){
@@ -53,8 +53,8 @@ public class Hand {
             return false;
         }
     }
-    
-	// retourne true si la somme des points a une valeur supérieur a 21. False sinon.
+
+    // retourne true si la somme des points a une valeur supérieur a 21. False sinon.
     public boolean isBust() {
         if (getPoints() > 21) {
             return true;
